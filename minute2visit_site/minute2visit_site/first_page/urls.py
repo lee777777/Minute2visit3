734da1cafce13a82b8d771a3929d1e2b1cde719c
lee777777 <leen99belal@gmail.com>
2023-08-16 22:21:32 +0400

    fixid a mistake

--- minute2visit_site/minute2visit_site/first_page/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("services/", views.services_page, name="services"),
-        path("service/", views.service, name="service"),
-]
