782a9bc9a48304094ecdbd018e25b0ffd780d48a
lee777777 <leen99belal@gmail.com>
2023-09-21 22:57:46 +0400

    made description show for only a count of words in services page, added the lowest price tag

---
+++ minute2visit_site/first_page/templatetags/truncate_words.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def truncate_words(value, num_words):
+    words = value.split()
+    if len(words) <= num_words:
+        return value
+    return ' '.join(words[:num_words]) 
