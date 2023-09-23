9b0500df59b96c58f950411c7a662cede25fb80c
lee777777 <leen99belal@gmail.com>
2023-09-24 01:16:04 +0400

    fixed design stuff in service page

--- minute2visit_site/first_page/templatetags/truncate_words.py
+++ minute2visit_site/first_page/templatetags/truncate_words.py
@@ -8,3 +8,10 @@ def truncate_words(value, num_words):
     if len(words) <= num_words:
         return value
     return ' '.join(words[:num_words]) 
+
+@register.filter
+def remaining_words(value, num_words):
+    words = value.split()
+    if len(words) <= num_words:
+        return ""
+    return ' '.join(words[num_words:]) 
